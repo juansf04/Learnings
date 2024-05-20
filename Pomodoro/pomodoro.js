307b9ebc09107196f350ac28b06c582b2ea96a3d
juansf04 <165486212+juansf04@users.noreply.github.com>
2024-05-20 12:46:51 -0300

    finalizado pomodoro

--- Pomodoro/pomodoro.js
+++ Pomodoro/pomodoro.js
@@ -1,27 +1,53 @@
 var time = 25 * 60;
 var timerInterval;
-var currentMode;
+var currentMode = "pomodoro";
+var totalBreaks = 0;
+
 var MODES = {
     pomodoro: 25,
     short: 5,
     long: 15,
 }
 
+const DEFAULT_MODES = {
+    pomodoro: 25,
+    short: 5,
+    long: 15,
+}
 
 document.querySelectorAll("#modes button")
     .forEach(button => {
         button.addEventListener('click', handleModeButtons)
     });
 
+document.querySelectorAll("#duration-control input")
+    .forEach(function(input){
+        input.addEventListener("change", durationControlHandler);
+        input.value = "";
+    });
+
+function durationControlHandler(event){
+    var value = event.target.value.trim();
+    var durationId = event.target.dataset.durationId;
+
+    if(value != '' && !isNaN(value) && Number.isInteger(parseFloat(value)) && parseInt(value) != 0) {
+        MODES[durationId] = parseInt(value);
+    } else {
+        MODES[durationId] = DEFAULT_MODES[durationId];
+    }
+
+    resetTimer();
+}
+
 function handleModeButtons(event) {
     switchMode(event.target.dataset.modeId);
 }
 
-function updateControlButtons(isrunning) {
+function updateControlButtons(isRunning) {
     var start_button = document.querySelector(".timer-control.start");
     var pause_button = document.querySelector(".timer-control.pause");
 
-    if(isrunning) {
+    if(isRunning) {
         start_button.disabled = true;
         pause_button.disabled = false;     
     } else {
@@ -31,31 +57,36 @@ function updateControlButtons(isrunning) {
 }
 
 function switchMode(mode) {
-
     currentMode = mode;
-    document.documentElement.style.backgroundColor = "var(--" + mode + ")"
+    document.documentElement.style.backgroundColor = "var(--" + mode + ")";
+    document.querySelectorAll("#modes button")
+        .forEach(elem => {
+            elem.classList.remove("active");        
+        });
+    document.querySelector(`button[data-mode-id="${mode}"]`).classList.add('active');   
     resetTimer();
 }
 
-
 function startTimer() {
-    timerInterval = setInterval(updateTimer, 1000)
+    timerInterval = setInterval(updateTimer, 1000);
     updateControlButtons(true);
 }
+
 function pauseTimer() {
     clearInterval(timerInterval);
     updateControlButtons(false);
 }
 
 function updateTimer() {
-
-    var minutes = Math.floor(time / 60); // 2
-    var seconds = time % 60; // 5
+    var minutes = Math.floor(time / 60);
+    var seconds = time % 60;
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     document.getElementById("timer").textContent = minutes + ":" + seconds;
+    document.title = `${minutes}:${seconds} - Pomodoro`;
+
     if(time <= 0) {
         pauseTimer();
         alert("Time's up!");
@@ -63,7 +94,6 @@ function updateTimer() {
         resetTimer();
     }
     time -= 1;
-
 }
 
 function nextMode() {
